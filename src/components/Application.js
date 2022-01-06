acb8ca88ea24ed0dc667b69a4271aa3f23e12162
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2022-01-05 20:06:05 -0500

    Install axios, import useEffect, axios in Application.js, comment out the days array, add days state, set days state from an api call using useEffect and axios.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,29 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import "components/Appointment"
 import DayList from "./DayList";
 import Appointment from "components/Appointment/index";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
+
+
 
 const appointments = {
   "1": {
@@ -65,6 +67,14 @@ const appointments = {
 
 
 export default function Application(props) {
+
+  const [days, setDays] = useState([]);
+    useEffect(()=>{
+      axios
+        .get("http://localhost:8001/api/days")
+        .then((response)=> setDays(response.data))
+    }, []
+  )
   
   const [day, setDay] = useState("Monday");
   return (
