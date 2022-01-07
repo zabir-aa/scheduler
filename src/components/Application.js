400f6a92a8020b637e71fcff2683105250295af9
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2022-01-07 02:59:08 -0500

    Add interviewewr state and update state with API call

--- src/components/Application.js
+++ src/components/Application.js
@@ -13,7 +13,8 @@ export default function Application(props) {
   const [state,setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   })
 
   const setDay = (day) => {setState(prev => ({ ...prev, day }))};
@@ -21,9 +22,10 @@ export default function Application(props) {
   useEffect(()=>{
     Promise.all([
       axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments') 
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers')
     ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     })
   },[])
 
