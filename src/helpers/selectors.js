16c5a0fa3078b533477a14059838048487fc40e8
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2022-01-08 03:38:49 -0500

    Write tests, modify state object, Implement the getInterviewersForDay function, pass all tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -31,11 +31,11 @@
 
  const getAppointmentsForDay = function(state, day) {
   let AppointmentsForDay=[];
-  const matchedDay = state.days.filter(d=> d.name === day);
-  if (state.days.length === 0 || matchedDay.length === 0){
+  const selectedDayArray = state.days.filter(d=> d.name === day);
+  if (state.days.length === 0 || selectedDayArray.length === 0){
     return AppointmentsForDay;
   };
-  matchedDay[0].appointments.forEach(element => {
+  selectedDayArray[0].appointments.forEach(element => {
     AppointmentsForDay.push(state.appointments[element])
   });
   return AppointmentsForDay;
@@ -60,4 +60,97 @@ const getInterview = function(state, interview) {
 }
 
 
- module.exports = {getAppointmentsForDay, getInterview}
+const state = {
+  days: [
+    {
+      id: 1,
+      name: "Monday",
+      appointments: [1, 2, 3],
+      interviewers: [1, 3]
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      appointments: [4, 5],
+      interviewers: [2]
+    }
+  ],
+  appointments: {
+    "1": { id: 1, time: "12pm", interview: null },
+    "2": { id: 2, time: "1pm", interview: null },
+    "3": {
+      id: 3,
+      time: "2pm",
+      interview: { student: "Archie Cohen", interviewer: 2 }
+    },
+    "4": { id: 4, time: "3pm", interview: null },
+    "5": {
+      id: 5,
+      time: "4pm",
+      interview: { student: "Chad Takahashi", interviewer: 2 }
+    }
+  },
+  interviewers: {
+    "1": {  
+      "id": 1,
+      "name": "Sylvia Palmer",
+      "avatar": "https://i.imgur.com/LpaY82x.png"
+    },
+    "2": {
+      id: 2,
+      name: "Tori Malcolm",
+      avatar: "https://i.imgur.com/Nmx0Qxo.png"
+    },
+    "3": {
+      id: 3,
+      name: "Tori Twin",
+      avatar: "https://i.imgur.com/Nmx0Qxo.png"
+    }
+  }
+};
+
+const getInterviewersForDay = function(state, day) {
+  let InterviewersForDay=[];
+  const selectedDayArray = state.days.filter(d=> d.name === day);
+  console.log(selectedDayArray);
+  if (state.days.length === 0 || selectedDayArray.length === 0){
+    return [];
+  } else {
+    selectedDayArray[0].interviewers.forEach(element => {
+      InterviewersForDay.push(state.interviewers[element])
+    });
+  }
+  return InterviewersForDay;
+};
+
+
+
+// const getInterviewersForDay = function(state, day) {
+//   let InterviewersForDay= [];
+//   let AppointmentIDsForDay = [];
+//   let interviewerIDsForDay = [];
+  
+//   const selectedDayArray = state.days.filter(d=> d.name === day)[0];
+
+
+//   if(selectedDayArray.appointments.length != 0) {
+//     AppointmentIDsForDay = selectedDayArray.appointments
+//   }
+
+//   AppointmentIDsForDay.forEach(element => {
+//     if(state.appointments[element].interview != null) {
+//       interviewerIDsForDay.push(state.appointments[element].interview.interviewer)}})
+  
+//   interviewerIDsForDay.forEach(element => {
+//     InterviewersForDay.push(state.interviewers[element])
+//   })
+    
+//   return InterviewersForDay;
+// };
+
+// console.log(getInterviewersForDay(state,"Wed"));
+
+
+
+
+ module.exports = {getAppointmentsForDay, getInterview, getInterviewersForDay}
--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -1,16 +1,18 @@
-import {getAppointmentsForDay, getInterview} from "helpers/selectors";
+import {getAppointmentsForDay, getInterview, getInterviewersForDay} from "helpers/selectors";
 
 const state = {
   days: [
     {
       id: 1,
       name: "Monday",
-      appointments: [1, 2, 3]
+      appointments: [1, 2, 3],
+      interviewers: [1, 3]
     },
     {
       id: 2,
       name: "Tuesday",
-      appointments: [4, 5]
+      appointments: [4, 5],
+      interviewers: [2]
     }
   ],
   appointments: {
@@ -38,9 +40,16 @@ const state = {
       id: 2,
       name: "Tori Malcolm",
       avatar: "https://i.imgur.com/Nmx0Qxo.png"
+    },
+    "3": {
+      id: 3,
+      name: "Tori Twin",
+      avatar: "https://i.imgur.com/Nmx0Qxo.png"
     }
   }
 };
+
+
 
 test("getAppointmentsForDay returns an array", () => {
   const result = getAppointmentsForDay(state, "Monday");
@@ -87,4 +96,30 @@ test("getInterview returns an object with the interviewer data", () => {
 test("getInterview returns null if no interview is booked", () => {
   const result = getInterview(state, state.appointments["2"].interview);
   expect(result).toBeNull();
+});
+
+
+test("getInterviewersForDay returns an array", () => {
+  const result = getInterviewersForDay(state, "Monday");
+  expect(Array.isArray(result)).toBe(true);
+});
+
+test("getInterviewersForDay returns an array with a length matching the number of interviewers for that day", () => {
+  const result = getInterviewersForDay(state, "Monday");
+  expect(result.length).toEqual(2);
+});
+
+test("getInterviewersForDay returns an array containing the correct interviewer objects", () => {
+  const [first] = getInterviewersForDay(state, "Tuesday");
+  expect(first).toEqual(state.interviewers["2"]);
+});
+
+test("getInterviewersForDay returns an empty array when the days data is empty", () => {
+  const result = getInterviewersForDay({ days: [] }, "Monday");
+  expect(result.length).toEqual(0);
+});
+
+test("getInterviewersForDay returns an empty array when the day is not found", () => {
+  const result = getInterviewersForDay(state, "Wednesday");
+  expect(result.length).toEqual(0);
 });
