5c407431637173e3cbd20cf0acbe92487d79de7f
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2022-01-07 22:35:56 -0500

    Correct wrongful object assignment as react child in Show.js, Correct appointments component call in Application.js, refactor Appointments component to implement visualModes EMPTY, CREATE, SHOW

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,18 +3,38 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from './Form';
+import useVisualMode from 'hooks/useVisualMode';
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE"
 
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );  
   return(
     <article className="appointment">
       <Header
         time = {props.time}
       />
-      {props.interview ? 
-      <Show {...props.interview} /> : <Empty />}
-      {/* {props.time? "" : "No"} Appointment{props.time? " at":"s"} {props.time} */}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && (
+        <Form 
+        interviewers={[]}
+        onSave={()=> transition(SHOW)} 
+        onCancel={back()}
+        onSubmit={transition(SHOW)}
+        />
+      )}
+      {mode === SHOW && (
+        <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer}
+        />
+      )}
     </article>
   )
 }
