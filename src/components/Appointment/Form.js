031578e66bdeaf3e3cf48b8c4006813e9d305378
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2022-01-10 03:21:51 -0500

    Refactor Form and Show component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-
 import Button from "../Button";
 import InterviewerList from "../InterviewerList";
 
@@ -8,29 +7,29 @@ export default function Form(props) {
   const [student, setStudent] = useState(props.userInput.student || props.student|| "");
   const [interviewer, setInterviewer] = useState(props.userInput.interviewer || props.interviewer || null);
 
-  const reset = ()=> {
-    setStudent("")
-    setInterviewer(null)
-  };
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  }
 
   return(
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-      <form autoComplete="off" onSubmit={event => event.preventDefault()}>
-      <input
-        className="appointment__create-input text--semi-bold"
-        name="name"
-        type="text"
-        placeholder="Enter Student Name"
-        value={student}
-        onChange={(event)=>{setStudent(event.target.value)}}
-      />
-      </form>
-      <InterviewerList 
-        interviewers={props.interviewers}
-        value={interviewer}
-        onChange={setInterviewer}
-      />
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event)=>{setStudent(event.target.value)}}
+          />
+        </form>
+        <InterviewerList 
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
       </section>
       <section className="appointment__card-right">
       <section className="appointment__actions">
