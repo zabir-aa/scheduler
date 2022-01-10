025dc665dcfcafce2da0b233234324d422f40d48
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2022-01-10 05:31:44 -0500

    Refactor stories index.js

--- stories/index.js
+++ stories/index.js
@@ -47,37 +47,37 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
   .add("Clickable", () => (<DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
 ));
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
   
-  storiesOf("DayList", module)
-  .addParameters({
-    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
-  })
-  .add("Monday", () => (
-    <DayList days={days} value={"Monday"} onChange={action("setDay")} />
-  ))
-  .add("Tuesday", () => (
-    <DayList days={days} value={"Tuesday"} onChange={action("setDay")} />
-  ))
-  .add("Wednesday", () => (
-    <DayList days={days} value={"Wednesday"} onChange={action("setDay")} />
-  ));
+storiesOf("DayList", module)
+.addParameters({
+  backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+})
+.add("Monday", () => (
+  <DayList days={days} value={"Monday"} onChange={action("setDay")} />
+))
+.add("Tuesday", () => (
+  <DayList days={days} value={"Tuesday"} onChange={action("setDay")} />
+))
+.add("Wednesday", () => (
+  <DayList days={days} value={"Wednesday"} onChange={action("setDay")} />
+));
 
 
 const interviewer = {
