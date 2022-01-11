ad1df891f0609bcc8a3b3caf9fc22e1dcf8a5d45
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2022-01-11 00:09:27 -0500

    Commented on hooks: useApplicationData functions (setDay, bookInterview, editInterview, cancelInterview), useVisualMode (mode, transition, back)

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,31 +1,31 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useApplicationData = function(){
+const useApplicationData = function(){ // Wrapper function to carry data management functions and state
 
-  const [state,setState] = useState({
+  const [state,setState] = useState({ // initial declaration of state with default values
     day: "Monday",
     days: [],
     appointments: {},
     interviewers: {}
   })
   
-  const setDay = (day) => {setState(prev => ({ ...prev, day }))};
+  const setDay = (day) => {setState(prev => ({ ...prev, day }))}; // Updates state whaen a new day is selected
   
-  function bookInterview(id, interview) {
-    const appointment = {
+  function bookInterview(id, interview) {  //function executes saving of a new appointment
+    const appointment = { // creates an appointment object with new interview data
       ...state.appointments[id],
       interview: { ...interview }
     };
   
-    const appointments = {
+    const appointments = { // creates an appointments object adding the new appointment
       ...state.appointments,
       [id]: appointment
     };
 
     const DAYS = []
 
-    for (const DAY of state.days) {
+    for (const DAY of state.days) { // creates a new days Array with updated spots
       if(DAY.appointments.includes(id)) {
         DAYS.push({...DAY, spots: DAY.spots-1}) 
       } else {
@@ -33,7 +33,7 @@ const useApplicationData = function(){
       }
     }
 
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, {interview})
+    return axios.put(`http://localhost:8001/api/appointments/${id}`, {interview}) // async put request to database server
     .then(()=> {
       setState({
         ...state,
@@ -43,18 +43,18 @@ const useApplicationData = function(){
     })
   }
 
-  function editInterview(id, interview) {
-    const appointment = {
+  function editInterview(id, interview) {  // function executes update on an existing appointment
+    const appointment = {  // creates an appointment object with updated interview data
       ...state.appointments[id],
       interview: { ...interview }
     };
   
-    const appointments = {
+    const appointments = {  // creates an appointments object with updated appointment
       ...state.appointments,
       [id]: appointment
     };
 
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, {interview})
+    return axios.put(`http://localhost:8001/api/appointments/${id}`, {interview})  // async put request to database server
     .then(()=> {
       setState({
         ...state,
@@ -63,19 +63,19 @@ const useApplicationData = function(){
     })
   }
   
-  function cancelInterview (id) {
-    const appointment = {
+  function cancelInterview (id) {  // function executes removal on an existing appointment
+    const appointment = {  // creates an appointment object with null interview data
       ...state.appointments[id],
       interview: null
     };
     
-    const appointments = {
+    const appointments = {  // creates an appointments object with updated appointment
       ...state.appointments,
       [id]: appointment
     };
 
     const DAYS = []
-    for (const DAY of state.days) {
+    for (const DAY of state.days) {  // creates a new days Array with updated spots
       if(DAY.appointments.includes(id)) {
         DAYS.push({...DAY, spots: DAY.spots+1})
       } else {
@@ -83,7 +83,7 @@ const useApplicationData = function(){
       }
     }
   
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`, {interview: null})
+    return axios.delete(`http://localhost:8001/api/appointments/${id}`, {interview: null})  // async delete request to database server
     .then(()=> {
       setState({
         ...state,
@@ -93,7 +93,7 @@ const useApplicationData = function(){
     })
   }
 
-  useEffect(()=>{
+  useEffect(()=>{  // makes 3 async get requests, and updates the state with all received data at once via primise.all
     Promise.all([
       axios.get('http://localhost:8001/api/days'),
       axios.get('http://localhost:8001/api/appointments'),
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,15 +2,17 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
 
-  const [history, setHistory] = useState([initial]);
+  const [history, setHistory] = useState([initial]); // Keeps a history of view modes for the appointment component
 
-  function transition(newMode, replace = false) {
+  // function to transition from one mode to the next, Replace = true replaces the last mode in history with the new one
+  function transition(newMode, replace = false) { 
     setHistory((prev)=> replace === false ? [...prev, newMode]: [...prev.slice(0,-1),newMode])
   }
   
-  function back(){
+  // function to return to the last mode in history
+  function back(){ 
     setHistory ((prev)=> prev.length > 1 ? prev.slice(0,-1): prev)
   }
 
-  return { mode: history[history.length-1], transition, back };
+  return { mode: history[history.length-1], transition, back }; // mode is returned as an element from history
 }
